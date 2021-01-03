c81512046bac7fe24b5bbc24d9d52effe44133c7
PHP Static <phpstatic.com@gmail.com>
2021-01-03 15:11:02 +0800

    add xxhash64_base62

--- php_xxhash.c
+++ php_xxhash.c
@@ -28,6 +28,41 @@ PHP_MINFO_FUNCTION(xxhash)
 	php_info_print_table_end();
 }
 
+static char * base62_digits="0123456789"
+                            "abcdefghijklmnopqrstuvwxyz"
+                            "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+
+static char *b62_encode(uint64_t num)
+{
+    /* 64 bits is up to 11 digits in base 62 */
+    static char enc[15] ;
+    int         i, len ;
+    uint64_t    rem ;
+    char        c ;
+
+    /* Zero out output buffer */
+    memset(enc, 0, sizeof(enc));
+    if (num==0) {
+        enc[0]='0';
+        return enc;
+    }
+    i=0 ;
+    while (num>0) {
+        rem = num % 62 ;
+        enc[i] = base62_digits[rem];
+        i++ ;
+        num /= 62 ;
+    }
+    /* Reverse string */
+    len = (int)strlen(enc);
+    for (i=0 ; i<len/2 ; i++) {
+        c = enc[i] ;
+        enc[i] = enc[len-i-1];
+        enc[len-i-1] = c ;
+    }
+    return enc ;
+}
+
 PHP_FUNCTION(xxhash32)
 {
     char *arg = NULL;
@@ -70,9 +105,31 @@ PHP_FUNCTION(xxhash64)
 	RETURN_STR(strg);
 }
 
+PHP_FUNCTION(xxhash64_base62)
+{
+	char *arg = NULL;
+    size_t arg_len, len;
+    zend_string *strg;
+	unsigned long long sum;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "s", &arg, &arg_len) == FAILURE || arg_len < 1) {
+        return;
+    }
+
+	// compute the checksum
+	sum = XXH64(arg, arg_len, 0);
+
+	//convert to a hex string
+	strg = strpprintf(0, "%s", b62_encode(sum));
+
+	// return the checksum
+	RETURN_STR(strg);
+}
+
 const zend_function_entry xxhash_functions[] = {
 	ZEND_FE(xxhash32, NULL)
 	ZEND_FE(xxhash64, NULL)
+	ZEND_FE(xxhash64_base62, NULL)
 	PHP_FE_END
 };
 
